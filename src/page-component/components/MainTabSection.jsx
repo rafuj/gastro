010b84c19c3e4f7401f2c8cad95830a7b3c42738
rafuj <raihanrafuj9978@gmail.com>
2024-12-11 22:22:07 +0600

    Fixed Guest Count

--- src/page-component/components/MainTabSection.jsx
+++ src/page-component/components/MainTabSection.jsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import React, { useEffect } from "react";
+import React from "react";
 import { DrinksMenu } from "./DrinksMenu";
 import LaCarteContent from "./LaCarteContent";
 import { MenuCarte } from "./MenuCarte";
@@ -9,11 +9,11 @@ export const MainTabSection = () => {
 	const [value, setValue] = React.useState(1);
 
 	const [guests, setGuests] = React.useState(9);
-	useEffect(() => {
-		if (value == 1) {
-			setGuests(15);
-		}
-	}, [value]);
+	// useEffect(() => {
+	// 	if (value == 1) {
+	// 		setGuests(15);
+	// 	}
+	// }, [value]);
 
 	return (
 		<Box sx={{ width: "100%", px: 3 }}>
