b04dd360b4e4742ede6e0e4c1f493da3581eb64e
rafuj <raihanrafuj9978@gmail.com>
2024-12-11 20:32:06 +0600

    Fix State Reset

--- src/page-component/components/MainTabSection.jsx
+++ src/page-component/components/MainTabSection.jsx
@@ -6,22 +6,39 @@ import { MenuCarte } from "./MenuCarte";
 import NewStandardMenu from "./NewStandardMenu";
 
 export const MainTabSection = () => {
-  const [value, setValue] = React.useState(1);
+	const [value, setValue] = React.useState(1);
 
-  const [guests, setGuests] = React.useState(9);
-  useEffect(() => {
-    if (value == 1) {
-      setGuests(15);
-    }
-  }, [value]);
+	const [guests, setGuests] = React.useState(9);
+	useEffect(() => {
+		if (value == 1) {
+			setGuests(15);
+		}
+	}, [value]);
 
-  return (
-    <Box sx={{ width: "100%", px: 3 }}>
-      <MenuCarte {...{ guests, setGuests, value, setValue }} />
-      {value === 0 && <LaCarteContent referanceGuest={guests} />}
-      {/* {value === 1 && <MenuContent guests={guests} />} */}
-      {value === 1 && <NewStandardMenu referanceGuest={guests} />}
-      {value === 2 && <DrinksMenu />}
-    </Box>
-  );
+	return (
+		<Box sx={{ width: "100%", px: 3 }}>
+			<MenuCarte {...{ guests, setGuests, value, setValue }} />
+			<Box
+				sx={{
+					display: value === 0 ? "block" : "none",
+				}}
+			>
+				<LaCarteContent referanceGuest={guests} />
+			</Box>
+			<Box
+				sx={{
+					display: value === 1 ? "block" : "none",
+				}}
+			>
+				<NewStandardMenu referanceGuest={guests} />
+			</Box>
+			<Box
+				sx={{
+					display: value === 2 ? "block" : "none",
+				}}
+			>
+				<DrinksMenu />
+			</Box>
+		</Box>
+	);
 };
